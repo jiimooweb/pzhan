d9585166316834e15159bd194613dd6e66133164
FanWaterWater <598804008@qq.com>
2018-10-17 11:12:16 +0800

    commit

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -41,4 +41,19 @@ class Picture extends Model
         return $this->belongsToMany(Fan::class, 'fan_like_pictures', 'picture_id', 'fan_id');
     }
 
+    public static function refershRank()
+    {   
+        //收藏排行
+        $collectRank = self::withCount(['collectFans'])->orderBy('collect_fans_count', 'desc')->limit(100)->get();
+        \Cache::store('redis')->put('collectRank', $collectRank, 30);
+
+        //点赞排行
+        $likeRank = self::withCount(['likeFans'])->orderBy('like_fans_count', 'desc')->limit(100)->get();
+        \Cache::store('redis')->put('likeRank', $likeRank, 30);
+
+        //热度排行
+        $hotRank = self::orderBy('hot', 'desc')->limit(100)->get();
+        \Cache::store('redis')->put('hotRank', $hotRank, 30);
+    }
+
 }
