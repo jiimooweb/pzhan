033603cfa51e2a059d35d416795dc4dfef04682f
FanWaterWater <598804008@qq.com>
2018-11-01 10:33:11 +0800

    commit

--- app/Http/Controllers/Api/Fans/NoticeController.php
+++ app/Http/Controllers/Api/Fans/NoticeController.php
@@ -24,13 +24,14 @@ class NoticeController extends Controller
     public function comment() 
     {
         $fan_id = request('fan_id') ?? Token::getUid();
-        $notices = CommentNotice::where('fan_id',$fan_id)->with(['fan', 'fromFan', 'toFan'])->paginate(20); 
+        $notices = CommentNotice::where('fan_id',$fan_id)->with(['fan', 'fromFan', 'toFan'])->paginate(10); 
         foreach($notices as &$notice) {
             if($notice->module == Module::Social) {
                 $notice->module_content = Social::where('id', $notice->module_id)->with('fan')->first(); 
             }
         }
-
+        CommentNotice::where(['fan_id' => $fan_id, 'status' => 0])->update(['status' => 1]);
+    
         return response()->json(['status' => 'success', 'data' => $notices]);  
     }
 
@@ -38,12 +39,13 @@ class NoticeController extends Controller
     public function like() 
     {
         $fan_id = request('fan_id') ?? Token::getUid();
-        $notices = LikeNotice::where('fan_id',$fan_id)->with(['fan', 'fromFan'])->paginate(20); 
+        $notices = LikeNotice::where('fan_id',$fan_id)->with(['fan', 'fromFan'])->paginate(10); 
         foreach($notices as &$notice) {
             if($notice->module == Module::Social) {
                 $notice->module_content = Social::where('id', $notice->module_id)->with('fan')->first(); 
             }
         }
+        LikeNotice::where(['fan_id' => $fan_id, 'status' => 0])->update(['status' => 1]);
 
         return response()->json(['status' => 'success', 'data' => $notices]);  
     }
