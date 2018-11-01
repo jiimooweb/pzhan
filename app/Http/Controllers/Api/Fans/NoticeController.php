d1ddd90d4c419bbe6d250ba9fabc21510bcd4732
FanWaterWater <598804008@qq.com>
2018-11-01 11:22:25 +0800

    commit

--- app/Http/Controllers/Api/Fans/NoticeController.php
+++ app/Http/Controllers/Api/Fans/NoticeController.php
@@ -24,7 +24,7 @@ class NoticeController extends Controller
     public function comment() 
     {
         $fan_id = request('fan_id') ?? Token::getUid();
-        $notices = CommentNotice::where('fan_id',$fan_id)->with(['fan', 'fromFan', 'toFan'])->paginate(10); 
+        $notices = CommentNotice::where('fan_id',$fan_id)->with(['fan', 'fromFan', 'toFan'])->orderBy('created_at', 'desc')->paginate(10); 
         foreach($notices as &$notice) {
             if($notice->module == Module::Social) {
                 $notice->module_content = Social::where('id', $notice->module_id)->with('fan')->first(); 
@@ -39,7 +39,7 @@ class NoticeController extends Controller
     public function like() 
     {
         $fan_id = request('fan_id') ?? Token::getUid();
-        $notices = LikeNotice::where('fan_id',$fan_id)->with(['fan', 'fromFan'])->paginate(10); 
+        $notices = LikeNotice::where('fan_id',$fan_id)->with(['fan', 'fromFan'])->orderBy('created_at', 'desc')->paginate(10); 
         foreach($notices as &$notice) {
             if($notice->module == Module::Social) {
                 $notice->module_content = Social::where('id', $notice->module_id)->with('fan')->first(); 
