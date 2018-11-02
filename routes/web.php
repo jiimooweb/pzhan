9eb6532dfa90628138a5e395e9101d79cd6f19ec
jimocc <245629490@qq.com>
2018-11-02 10:39:24 +0800

    备份

--- routes/web.php
+++ routes/web.php
@@ -152,5 +152,6 @@ Route::group(['middleware' => ['cors', 'token']], function () {
 
     //专题评论
     Route::get('specials/{special}/comments', 'Api\Specials\SpecialCommentController@getcomments');
+    Route::post('specials/{special}/comment', 'Api\Specials\SpecialCommentController@comment');
 
 });
