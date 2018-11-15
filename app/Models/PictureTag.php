b835ac34d8fb9c0833657043138425642f14de00
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-15 19:57:23 +0800

    commit

--- app/Models/PictureTag.php
+++ app/Models/PictureTag.php
@@ -18,7 +18,7 @@ class PictureTag extends Model
         $tags = self::where('picture_id',$picture_id)->get()->pluck('tag_id');
         $picture_ids = self::whereIn('tag_id', $tags)->whereNotIn('picture_id', [$picture_id])->inRandomOrder()->limit($limit)->get()->pluck('picture_id');
         
-        $recommends = Picture::whereIn('id', $picture_ids)->with(['tags'])->get();
+        $recommends = Picture::whereIn('id', $picture_ids)->with(['tags'])->withCount(['collectFans','likeFans'])->get();
         foreach($recommends as &$recommend) {
             $recommend->collect = $recommend->isCollect($fan_id) ? 1 : 0;
             $recommend->like = $recommend->isLike($fan_id) ? 1 : 0;
