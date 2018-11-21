cd2956292dd4a92ac4aa2eb4e6ddd1fda44f6ee4
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-11-21 15:35:39 +0800

    commit

--- app/Http/Controllers/Api/Fans/FanController.php
+++ app/Http/Controllers/Api/Fans/FanController.php
@@ -7,6 +7,7 @@ use App\Models\Sign;
 use App\Models\Picture;
 use App\Services\Token;
 use EasyWeChat\Factory;
+use App\Models\FanShare;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cache;
