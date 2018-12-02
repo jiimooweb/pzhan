fc256394fe2e11d284ee6b0cc9d540f6cab0f645
翻水水 <wx_2e929a573f6147d0b059920bc5907022@git.code.tencent.com>
2018-12-02 18:58:15 +0800

    commit

---
+++ app/Http/Controllers/Api/Ads/AdController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api\Ads;
+
+use App\Models\Ad;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdController extends Controller
+{
+    public function index()
+    {
+        $ads = Ad::get();
+        return response()->json(['status' => 'success', 'data' => $ads]);                            
+    }
+
+    public function store()
+    {
+        $data = request()->all();
+        
+        if(Ad::create($data)) {
+            return response()->json(['status' => 'success', 'msg' => '新增成功！']);                            
+        }
+            
+        return response()->json(['status' => 'error', 'msg' => '新增失败！']);  
+    }
+
+    public function update()
+    {
+        $data = request()->all();
+        
+        if(Ad::where('id', request()->ad)->update($data)) {
+            return response()->json(['status' => 'success', 'msg' => '新增成功！']);                            
+        }
+            
+        return response()->json(['status' => 'error', 'msg' => '新增失败！']); 
+    }
+
+    public function destroy()
+    {
+        if(Ad::where('id', request()->ad)->delete()) {
+            return response()->json(['status' => 'success', 'msg' => '删除成功！']);               
+        }
+
+        return response()->json(['status' => 'error', 'msg' => '删除失败！']);  
+    }
+
+    public function app()
+    {
+        $ads = Ad::where('hidden', 0)->get();
+        return response()->json(['status' => 'success', 'data' => $ads]); 
+    }
+}
+
