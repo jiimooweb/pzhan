8f4076a47ffe801ee7948b1ed95ef6e267ec48e1
jimocc <245629490@qq.com>
2019-03-13 11:37:02 +0800

    备份

--- app/Http/Controllers/Api/Configs/ConfigController.php
+++ app/Http/Controllers/Api/Configs/ConfigController.php
@@ -13,7 +13,7 @@ class ConfigController extends Controller
         $data = [];
         $data['me_reward_switch'] = 1 ; //我的 - 打赏支持
         $data['index_img_switch'] = 1 ; //首页 图片开关 暂时用于打赏
-        $data['index_img'] = ""; //首页图片url
+        $data['index_img'] = "https://download.rdoorweb.com/reward.png"; //首页图片url
         $data['index_url'] = 0; // 首页图片跳转链接 为0默认跳转打赏小程序
         return response()->json(['status' => 'success', 'data' => $data]);
 
